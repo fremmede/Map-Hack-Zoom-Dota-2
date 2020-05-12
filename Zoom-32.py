6e2de2057c8948c49b195f164c6a3fdc143cef07
Fremder <thake.viral@gmail.com>
2020-05-11 19:58:32 -0500

    Update and rename Zoom-esp.py to Zoom-32.py

---
+++ Zoom-32.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2.7.6
+import re, time
+lib = "client.dll"
+with open(lib, mode='rb') as s:
+    d = s.readlines()
+
+
+with open(lib, mode='rb') as r:
+    strings = re.findall('\d{4}\x00\x00\x00\x00Maximum visible distance', r.read())
+    c = ''.join(strings)[0:4]
+    print "Your size =", c
+
+
+def complete():
+  with open(lib, mode='wb') as outfile:
+    for line in d:
+      res = re.compile('\d{4}\x00\x00\x00\x00Maximum visible distance')
+      cam = res.sub((str(x)) +'\x00\x00\x00\x00Maximum visible distance', line)
+      outfile.write(cam)
+
+print "Type 4-digits number"
+while True:
+  try:
+    x = int(raw_input())
+    if len(str(x)) == 4:
+      print "Complete""\n", "Your new size is:", x
+      complete()
+      time.sleep(6)
+      exit()
+    else:
+      print "Only 4-digit numbers"
+  except ValueError:
+    print "No words, only numbers"
