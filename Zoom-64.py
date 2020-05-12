f22076f157f336ec6d70cbd0de311e3608d94038
Fremder <thake.viral@gmail.com>
2020-05-11 20:02:17 -0500

    Update and rename Zoom.py to Zoom-64.py

--- Zoom-64.py
+++ Zoom-64.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python2.7.6
 import re, time
-filename = "client.dll"
-with open(filename, mode='rb') as s:
+lib = "client.dll"
+with open(lib, mode='rb') as s:
     d = s.readlines()
 
 
-with open(filename, mode='rb') as r:
+with open(lib, mode='rb') as r:
     strings = re.findall('\d{4}\x00\x00\x00\x00\x00\x00\x00\x00r_propsmaxdist', r.read())
     c = ''.join(strings)[0:4]
     print "Your size =", c
 
 
 def complete():
-  with open(filename, mode='wb') as outfile:
+  with open(lib, mode='wb') as outfile:
     for line in d:
       res = re.compile('\d{4}\x00\x00\x00\x00\x00\x00\x00\x00r_propsmaxdist')
       cam = res.sub((str(x)) +'\x00\x00\x00\x00\x00\x00\x00\x00r_propsmaxdist', line)
@@ -25,13 +25,9 @@ while True:
     if len(str(x)) == 4:
       print "Complete""\n", "Your new size is:", x
       complete()
-      time.sleep(3)
+      time.sleep(6)
       exit()
     else:
       print "Only 4-digit numbers"
   except ValueError:
     print "No words, only numbers"
-
-
-
-
